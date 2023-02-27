98c58ac94bd577c7306939e397cdb9438567828b
Jafet Leon-Perez <78179738+jleon124@users.noreply.github.com>
2023-02-27 10:45:38 -0500

    Creates Layout.js for routing

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div className="App">
+        <Header />
+        <Outlet />
+        <Footer />
+    </div>
+  )
+}
+
+export default Layout
