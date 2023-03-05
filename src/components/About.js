32765e713775659033982a526e97365d9e22e525
Saif <sshaikh4@ggc.edu>
2023-03-05 18:13:11 -0500

    changes to about

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,239 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+
+    <>
+    <body id='About_page'>
+    <h1 class="about_title_page">CyberProGames</h1>
+
+    <br></br><br></br>
+
+    <img src= '/cpg_logo.png' alt="CyberProGames logo" width='300' height='300' id="about_logo"/>
+       
+
+    <div class="about_description">
+      <p class="about_text">
+        About the projectr and team blah blah  <br></br>
+        blah blah blah blah blah blah blah blah <br></br>
+        blah blah blah blah blah blah blah blah <br></br>
+        blah blah blah blah blah blah blah blah <br></br>
+        blah blah blah blah blah blah blah blah <br></br>
+        blah blah blah blah blah blah blah blah <br></br>
+        blah blah blah blah blah blah blah blah <br></br>
+
+   </p>
+      
+    </div>
+
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+
+
+
+    <div class="box_profile_img">
+        <img src= '/miranda_pic.png' alt="souvenir"></img>
+
+    </div>
+    <div class="profile_about_Box">
+      <div>
+          <p class="profile_desciptions">
+          Name: <h class="color_text">Miranda Darlington</h> <br></br>
+          Gender: <h class="color_text">Unknown</h><br></br>
+          Age: <h class="color_text">Unknown</h><br></br>
+          Height: <h class="color_text">Unknown</h><br></br>
+          Skills: <h class="color_text">Unknown</h> <br></br>
+          <br></br>
+          Role: <h class="color_text">Unknown</h><br></br>
+          Social content: <h class="color_text"><a href="https://www.linkedin.com/in/m1darl1ng/" target="blank" >LinkedIn</a></h>
+          </p>
+        
+        </div>
+    </div>
+
+
+   
+
+    <div class="white_profileBox"></div>
+    
+
+
+  
+    <div class="box_profile_img_right">
+      <img src= '/jafet_pic.png' alt="souvenir"/>
+
+    </div>
+    <div class="profileBox_right_side">
+        <div>
+        <p class="profile_desciptions_right">
+          Name: <h class="color_text">Jafet Leon-Perez</h> <br></br>
+          Gender: <h class="color_text">Unknown</h><br></br>
+          Age: <h class="color_text">Unknown</h><br></br>
+          Height: <h class="color_text">Unknown</h><br></br>
+          Skills: <h class="color_text">Unknown</h> <br></br>
+          <br></br>
+          Role: <h class="color_text">Unknown</h><br></br>
+          Social content: <h class="color_text"><a href="https://www.linkedin.com/in/jafet-leon-perez/" target="blank">LinkedIn</a></h>
+          </p>
+      
+        </div>
+    </div>
+
+
+
+      
+    <div class="white_profileBox"></div>
+    <div class="white_profileBox"></div>
+      
+
+
+    
+    <div class="box_profile_img">
+        <img src= '/person_holder.png' alt="souvenir"/>
+    </div>
+    <div class="profileBox">
+        <div>
+        <p class="profile_desciptions">
+          Name: <h class="color_text">Saif Shaikh</h> <br></br>
+          Gender: <h class="color_text">Unknown</h><br></br>
+          Age: <h class="color_text">Unknown</h><br></br>
+          Height: <h class="color_text">Unknown</h><br></br>
+          Skills: <h class="color_text">Unknown</h> <br></br>
+          <br></br>
+          Role: <h class="color_text">Unknown</h><br></br>
+          Social content: <h class="color_text"><a href="https://www.linkedin.com/in/-saif-shaikh/" target="blank">LinkedIn</a></h>
+          </p>
+        
+        </div>
+    </div>
+
+
+
+      
+    <div class="white_profileBox"></div>
+      
+
+
+
+    <div class="box_profile_img_right">
+        <img src= '/Javier_pic.png' alt="souvenir" />
+    </div>
+    <div class="profileBox_right_side">
+        <div>
+        <p class="profile_desciptions_right">
+          Name: <h class="color_text">Javier Zarate Zaragoza</h> <br></br>
+          Gender: <h class="color_text">Unknown</h><br></br>
+          Age: <h class="color_text">Unknown</h><br></br>
+          Height: <h class="color_text">Unknown</h><br></br>
+          Skills: <h class="color_text">Unknown</h> <br></br>
+          <br></br>
+          Role: <h class="color_text">Unknown</h><br></br>
+          Social content: <h class="color_text"><a href="https://www.linkedin.com/in/javier-zarate-zaragoza-7b6817240/" target="blank">LinkedIn</a></h>
+          </p>
+        
+        </div>
+
+    </div>
+
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+
+    <img src= '/footer_razor_img.png' alt="footer" id ="footer_repeater"/>
+ 
+    <br></br>
+
+    <div class="footer_grid">
+      <div class="footer_grid_item">
+        <h1 class="footer_title">CyberProGames</h1>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+
+
+      </div>
+
+
+
+
+      <div class="footer_grid_item">
+      <h1 class="footer_title">Browse Games</h1>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        
+        
+      </div>
+
+
+
+
+
+
+      <div class="footer_grid_item">
+      <h1 class="footer_title">Place holder</h1>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        
+        
+      </div>
+
+
+
+
+
+
+      <div class="footer_grid_item">
+      <h1 class="footer_title">More</h1>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        
+        
+      </div>
+
+
+
+
+
+
+
+      <div class="footer_grid_item">
+      <h1 class="footer_title">Follow Us</h1>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        <p class="footer_text">blah</p>
+        
+        
+      </div>
+
+
+    </div>
+
+
+
+
+    </body>
+    </>
+
   )
 }
 
