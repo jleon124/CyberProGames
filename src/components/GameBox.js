a21ced848ffb89c64fe0db8673f869a4a7eb3c3c
Jafet Leon-Perez <78179738+jleon124@users.noreply.github.com>
2023-03-26 18:55:49 -0400

    Fixed homepage top section & created gamebox comp

---
+++ src/components/GameBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GameBox = () => {
+  return (
+    <div class="text-center bg-cyan-600 h-[250px] w-[200px] game-box">
+        <p>GameTitle</p>
+    </div>
+  )
+}
+
+export default GameBox
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,81 +1,51 @@
 import React from 'react'
+import GameBox from './GameBox'
 
 const HomePage = () => {
     return (
-        <main class="h-auto bg-[linear-gradient(to_bottom,#050b13,#11131e,#1c1727,#2b192e,#3c1b30,#431a2f,#4a192c,#511929,#511627,#501324,#501022,#4f0c1f)]">
-            <h1 class="text-[yellow] bg-black font-cyber">CyberProGames</h1>
-            <p>A website with a purpose of hosting various games ranging
-                from just-for-fun to game to learning about programming/computer
-                science.</p>
-            <div class="flex flex-wrap justify-evenly">
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game1</p>
-                </div>
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game2</p>
-                </div>
-                <div class="text-center bg-cyan-600 h-[500px] w-[500px] float-right leaderboard">
+        <main class=" pt-5 pb-5 h-auto bg-[linear-gradient(to_bottom,#050b13,#11131e,#1c1727,#2b192e,#3c1b30,#431a2f,#4a192c,#511929,#511627,#501324,#501022,#4f0c1f)]">
+            <h1 class="text-[yellow] font-cyber text-5xl mb-12 ml-36">CyberProGames</h1>
+
+            <div class="grid grid-cols-4 grid-rows-2 mx-36 ">
+
+                <p class=" col-span-2 text-white text-2xl ">A website with a purpose of hosting various games ranging
+                    from just-for-fun to game to learning about programming/computer
+                    science.</p>
+
+                <div class="justify-self-center col-span-2 row-span-2 text-center bg-cyan-600 h-[500px] w-[500px] leaderboard-box ">
                     <p>Leaderboard</p>
                 </div>
+
+                <div class=" col-span-2 flex justify-evenly">
+                    <GameBox />
+                    <GameBox />
+                </div>
+
             </div>
 
-            {/* <br></br> */}
-
-            <h1 class="text-[yellow] bg-black font-cyber">Trending</h1>
-
+            <h1 class="text-[yellow] font-cyber text-3xl mb-6 ml-36">Trending</h1>
             <div class="flex justify-evenly">
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game1</p>
-                </div>
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game2</p>
-                </div>
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game3</p>
-                </div>
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game4</p>
-                </div>
+                <GameBox />
+                <GameBox />
+                <GameBox />
+                <GameBox />
             </div>
 
-            {/* <br></br> */}
-
-            <h1 class="text-[yellow] bg-black font-cyber">Programming</h1>
+            <h1 class="text-[yellow] font-cyber text-3xl mb-6 ml-36">Programming</h1>
             <div class="flex justify-evenly">
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game1</p>
-                </div>
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game2</p>
-                </div>
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game3</p>
-                </div>
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game4</p>
-                </div>
+                <GameBox />
+                <GameBox />
+                <GameBox />
+                <GameBox />
             </div>
 
-            {/* <br></br> */}
-
-            <h1 class="text-[yellow] bg-black font-cyber">Fun</h1>
-            <div class="flex justify-evenly">
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game1</p>
-                </div>
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game2</p>
-                </div>
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game3</p>
-                </div>
-                <div class="text-center bg-cyan-600 h-[250px] w-[200px] m-5 gamebox">
-                    <p>Game4</p>
-                </div>
+            <h1 class="text-[yellow] font-cyber text-3xl mb-6 ml-36">Fun</h1>
+            <div class="flex justify-evenly ">
+                <GameBox />
+                <GameBox />
+                <GameBox />
+                <GameBox />
             </div>
-
-            {/* <hr></hr> */}
-
         </main>
     )
 }
