bdb1b958da3972c89b2f040c01137b27c0f7ae0a
Jafet Leon-Perez <78179738+jleon124@users.noreply.github.com>
2023-03-26 18:53:35 -0400

    Converted register and login page css to tw

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,27 +5,27 @@ const LoginPage = () => {
   const navigate = useNavigate();
 
   return (
-    <main className="formPages">
-      <div className="loginBox">
-        <h1 className="formTitle">Login</h1>
-        <form className="loginForm">
+    <main class="min-h-screen flex justify-center items-center">
+      <div class="h-[400px] w-[400px] bg-black/40 border-cyan-400 border-4 text-teal-400 p-9 login-box">
+        <h1 class="text-2xl text-[#1afa8d]">Login</h1>
+        <form class="flex flex-col">
           <input
-            className="inputEntry"
-            name="username"
-            placeholder="Username"
+            class="h-[30px] w-[250px] my-[10px] input-box text-black"
+            name="email"
+            placeholder=" Email"
             required
           />
           <input
-            className="inputEntry"
+            class="h-[30px] w-[250px] my-[10px] input-box text-black"
             name="password"
-            placeholder="Password"
+            placeholder=" Password"
             required
           />
-          <nav className="formButtonNav">
-            <button className="formButton" onClick={() => navigate("/")}>Back</button>
-            <button className="formButton" type="submit">Login</button>
+          <nav class="mt-[5px]">
+            <button class="text-black bg-emerald-700 border-[#1afa8d] border-2 h-[30px] w-[75px] mr-[5px] hover:bg-[#1afa8d] form-button-box" onClick={() => navigate("/")}>Back</button>
+            <button class="text-black bg-emerald-700 border-[#1afa8d] border-2 h-[30px] w-[75px] mr-[5px] hover:bg-[#1afa8d] form-button-box" type="submit">Login</button>
           </nav>
-          <a onClick={() => navigate("/register")} >Sign-up?</a>
+          <a class="text-[#1afa8d] mt-[10px] underline cursor-pointer hover:no-underline" onClick={() => navigate("/register")} >Sign-up?</a>
         </form>
       </div>
     </main>
--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -5,38 +5,32 @@ const RegisterPage = () => {
   const navigate = useNavigate();
 
   return (
-    <main className="formPages">
-      <div className="registerBox">
-        <h1 className="formTitle">Registration</h1>
-        <form className="registerForm">
+    <main class="min-h-screen flex justify-center items-center">
+      <div class="h-[400px] w-[400px] bg-black/40 border-cyan-400 border-4 text-teal-400 p-9 login-box">
+        <h1 class="text-2xl text-[#1afa8d]">Registration</h1>
+        <form class="flex flex-col">
           <input
-            className="inputEntry"
-            name="name"
-            placeholder="Name"
-            required
-          />
-          <input
-            className="inputEntry"
+            class="h-[30px] w-[250px] my-[10px] input-box text-black"
             name="username"
-            placeholder="Username"
+            placeholder=" Username"
             required
           />
           <input
-            className="inputEntry"
+            class="h-[30px] w-[250px] my-[10px] input-box text-black"
             name="email"
-            placeholder="Email"
+            placeholder=" Email"
             required
           />
           <input
-            className="inputEntry"
+            class="h-[30px] w-[250px] my-[10px] input-box text-black"
             name="password"
-            placeholder="Password"
+            placeholder=" Password"
             required
           />
-          <label for="humanVeri">Are you human? <input type="checkbox" name="human"/></label>
-          <nav className="formButtonNav">
-            <button className="formButton" onClick={() => navigate("/login")}>Back</button>
-            <button className="formButton" type="submit">Sign up</button>
+          <label class="text-white" for="humanVeri">Are you human? <input type="checkbox" name="human"/></label>
+          <nav class="mt-[5px]">
+            <button class="text-black bg-emerald-700 border-[#1afa8d] border-2 h-[30px] w-[75px] mr-[5px] hover:bg-[#1afa8d] form-button-box" onClick={() => navigate("/login")}>Back</button>
+            <button class="text-black bg-emerald-700 border-[#1afa8d] border-2 h-[30px] w-[75px] mr-[5px] hover:bg-[#1afa8d] form-button-box" type="submit">Sign up</button>
           </nav>
         </form>
       </div>
