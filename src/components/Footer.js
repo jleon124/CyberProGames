04e14aad002ea19618a5c24c4f7e3387f3512295
Jafet Leon-Perez <78179738+jleon124@users.noreply.github.com>
2023-03-26 18:56:17 -0400

    Adjusted footer formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,97 +1,58 @@
 import React from 'react'
 
 const Footer = () => {
+  return (
+    <footer>
+      <img src='/footer_razor_img.png' alt="footer" class="footer_repeater" />
 
-    return (
-        <footer>
-          
+      <br></br>
 
-  
-  <img src= '/footer_razor_img.png' alt="footer" class ="footer_repeater"/>
- 
- <br></br>
+      <div class="footer_grid">
+        <div class="footer_grid_item">
+          <h1 class="footer_title">CyberProGames</h1>
+          <p class="footer_text">About Us</p>
+          <p class="footer_text">Terms of use</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+        </div>
 
- <div class="footer_grid">
-   <div class="footer_grid_item">
-     <h1 class="footer_title">CyberProGames</h1>
-     <p class="footer_text">About Us</p>
-     <p class="footer_text">Terms of use</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
+        <div class="footer_grid_item">
+          <h1 class="footer_title">Browse Games</h1>
+          <p class="footer_text">Program games</p>
+          <p class="footer_text">Fun games</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+        </div>
 
+        <div class="footer_grid_item">
+          <h1 class="footer_title">More</h1>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+        </div>
 
-   </div>
+        <div class="footer_grid_item">
+          <h1 class="footer_title">Follow Us</h1>
+          <p class="footer_text">LinkedIn</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+          <p class="footer_text">blah</p>
+        </div>
 
+        <h1 class="rights_text">© 2023 CyberProGames <br/> All Rights Reserved. </h1>
 
-
-
-   <div class="footer_grid_item">
-   <h1 class="footer_title">Browse Games</h1>
-     <p class="footer_text">Program games</p>
-     <p class="footer_text">Fun games</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     
-     
-   </div>
-
-
-
-
-
-   <div class="footer_grid_item">
-   <h1 class="footer_title">More</h1>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     
-     
-   </div>
-
-
-
-
-
-
-
-   <div class="footer_grid_item">
-   <h1 class="footer_title">Follow Us</h1>
-     <p class="footer_text">LinkedIn</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     <p class="footer_text">blah</p>
-     
-     
-   </div>
-
-   <h1 class="rights_text">© 2023 CyberProGames.com All Rights Reserved. </h1>
-
- </div>
-
- 
-
- 
-
-  {/* <h1 class="rights_text">© 2023 CyberProGames.com All Rights Reserved. </h1> */}
-
-
-
-      
-
-
-
-
-        </footer>
-    )
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
