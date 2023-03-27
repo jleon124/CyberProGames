0c655822fbe29b0469b0eabe2ca0ff9352b1b6e7
Mimi <mimiehd@gmail.com>
2023-03-27 12:09:16 -0400

    Added in props functionality to GameBox.js

--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const GameBox = () => {
+const GameBox = ({title, src, alt, descp}) => {
   return (
-    <div class="text-center bg-cyan-600 h-[250px] w-[200px] game-box">
-        <p>GameTitle</p>
+    <div class="text-center bg-cyan-600 h-[250px] w-[200px] game-box relative">
+        <img className="" src = {src}  width={200} height={200} alt={alt}/>
+        <p class="absolute bottom-0 left-5">{title}</p>
     </div>
   )
 }
