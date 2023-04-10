d6524ee891b0aea60ea21ca80d703c86af22d597
Mimi <mimiehd@gmail.com>
2023-04-10 12:29:22 -0400

    fixed rating btn issue and made components

---
+++ frontend/src/components/Comments.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import React from "react";
+
+
+
+
+function Comments()
+{
+
+
+  return(
+    <div className='text-center bg-cyan-600 h-[250px] w-[600px] mt-2'>
+        <span>Comments</span>
+    </div>
+    
+
+
+  )
+
+}
+
+
+
+
+
+export default Comments;
--- frontend/src/components/FavBtn.js
+++ frontend/src/components/FavBtn.js
@@ -24,7 +24,7 @@ function FavBtn()
 
   return(
     <div className='pl-4'>
-        <button className = {` ${activeBtn === "heart" ? "bg-[yellow]" : "bg-black/40 text-[rgba(88,221,229,255)]"}  flex items-center justify-evenly h-10 w-[125px] p-[15px] bg-[rgba(44,17,25,0.6)] text-black border-[3px] border-[rgba(88,221,229,255)]`}
+        <button className = {` ${activeBtn === "heart" ? "bg-[yellow] text-black" : "bg-black/40 text-[rgba(88,221,229,255)]"}  flex items-center justify-evenly h-10 w-[125px] p-[15px] bg-[rgba(44,17,25,0.6)]  border-[3px] border-[rgba(88,221,229,255)]`}
         onClick={handleClick}>
             <BsFillHeartFill/> Favorite 
         </button>
