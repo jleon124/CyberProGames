b51c7573d9e11bdb1f4a2ca6c36dd9052f8f2732
Jafet Leon-Perez <78179738+jleon124@users.noreply.github.com>
2023-04-17 13:15:51 -0400

    Configure and add hooks for login and register

---
+++ frontend/src/hooks/useRegister.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useRegister = () => {
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+    const { dispatch } = useAuthContext()
+
+    const register = async (username, email, password) => {
+        setIsLoading(true)
+        setError(null)
+
+        const response = await fetch('http://localhost:3500/api/user/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, email, password })
+        })
+
+        const json = await response.json()
+
+        if (!response.ok) {
+            setIsLoading(false)
+            setError(json.error)
+        } else {
+            localStorage.setItem('user', JSON.stringify(json))
+            dispatch({ type: 'LOGIN', payload: json })
+            setIsLoading(false)
+        }
+    }
+
+    return { register, isLoading, error }
+}
