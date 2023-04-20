2ac97e203d715523d07bb30e09de78103f4b119c
Jafet Leon-Perez <78179738+jleon124@users.noreply.github.com>
2023-04-20 14:59:11 -0400

    Rework authentication and state with redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,27 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { AuthContextProvider } from './context/AuthContext'
+import authReducer from "./state"
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from 'react-redux'
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = { key: 'root', storage, version: 1 }
+const persistedReducer = persistReducer(persistConfig, authReducer)
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
-
-  // <React.StrictMode>
-  //   <AuthContextProvider>
-  //     <Router>
-  //       <Routes>
-  //         <Route path="/*" element={<App />} />
-  //       </Routes>
-  //     </Router>
-  //   </AuthContextProvider>
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
