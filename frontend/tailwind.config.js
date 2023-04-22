60a92235b64da94b23b1f13f3cb099c7c36ed501
Jafet Leon-Perez <78179738+jleon124@users.noreply.github.com>
2023-04-22 17:34:44 -0400

    Create bg tailwind styles for profile pics

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
         'cyber': ['Cyberpunk'],
         'valo': ["Valo"],
         'mono': ["Courier New"],
+      },
+      backgroundImage: {
+        'defaulticon': "url('/public/profilepictures/default_profilepic.png')",
+        'eyeicon': "url('/public/profilepictures/eye_profile.png')",
+        'glassesicon': "url('/public/profilepictures/glasses_profile.png')",
+        'grizicon': "url('/public/profilepictures/griz_profile.png')",
+        'imgicon': "url('/public/profilepictures/img_profile.png')",
+        'maskicon': "url('/public/profilepictures/mask_profilepic.png')",
+        'angyicon': "url('/public/profilepictures/angy_profilepic.png')",
       }
     },
   },
