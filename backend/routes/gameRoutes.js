4805deb937ec208fef05418aa987a79092f0e353
Jafet Leon-Perez <78179738+jleon124@users.noreply.github.com>
2023-04-25 16:55:58 -0400

    Update routes to include jwt verification

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const gameController = require('../controllers/gameController')
 const router = express.Router()
+const verifyJWT = require('../middleware/verifyJWT')
 
 // /game/
 router.get('/:gameId', gameController.getGameData)
-router.patch('/:gameId/liketoggle', gameController.updateLikeDislike)
-router.patch('/:gameId/favtoggle', gameController.favoriteGame)
+router.patch('/:gameId/liketoggle', verifyJWT, gameController.updateLike)
+router.patch('/:gameId/favtoggle', verifyJWT, gameController.favoriteGame)
 
 module.exports = router
